d7feba4abbcbad7770732a174f2cd50e08ee352a
CHIEH LO <flicker8815@gmail.com>
2021-03-10 20:51:57 +0800

    Tutorial 2

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -1,19 +1,27 @@
-#include <iostream>
-#include <string>
 #include "pch.h"
 #include "stdafx.h"
-
+#include <string>
+#include <iostream>
 using namespace System;
 
 bool Guess(int number) {
-	return false;
+    static int target = -1;
+
+    if (target == -1) {
+        Random r;
+        target = r.Next() % 100 + 1;
+    }
+
+    if (number == target)
+        return true;
+    return false;
 }
 
 int main(array<System::String^>^ args)
 {
-	int guess;
-	do {
-		std::cin >> guess;
-	} while (!Guess(guess));
-	return 0;
+    int guess;
+    do {
+        std::cin >> guess;
+    } while (!Guess(guess));
+    return 0;
 }
