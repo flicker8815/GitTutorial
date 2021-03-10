b9dabc642e6a7c26d777e00ae5b2207ecc8d2217
CHIEH LO <flicker8815@gmail.com>
2021-03-10 19:14:00 +0800

    Tutorial 2

---
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include "pch.h"
+//#include "stdafx.h"
+
+using namespace System;
+
+//bool Guess(int number) {
+
+//}
+
+int main(array<System::String^>^ args)
+{
+
+}
